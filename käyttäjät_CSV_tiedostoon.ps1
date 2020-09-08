4f2d6e3456eec7c490ad33251cecce2ddde696a8
MattiPiiroinen <60738652+MattiPiiroinen@users.noreply.github.com>
2020-09-08 14:27:38 +0300

    Add files via upload

---
+++ käyttäjät_CSV_tiedostoon.ps1
@@ -0,0 +1,10 @@
+﻿# Haetaan AD käyttäjän olennaiset tiedot
+
+# Kysytään käyttäjältä, minkä nimiseen tiedostoon tallennetaan käyttäjät
+$Tiedosto = Read-Host "Mihin tallennetaan, anna polku"
+
+# Luetaan AD-käyttäjät muuttujaan
+$Käyttäjät = Get-ADUser -Filter * -Properties AccountExpirationDate,Office,OfficePhone,DisplayName,Description,whenCreated 
+
+# Putkitetaan tiedot CSV-tiedostoon
+$Käyttäjät | Export-Csv -Encoding Unicode -Delimiter  "`t" -Path $Tiedosto
