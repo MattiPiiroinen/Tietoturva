d4b1b77bf567845e3e6518252e3c045f8d2688df
Matti <mpiiroinen27@gmail.com>
2020-09-15 11:32:26 +0300

    Siirretty tiedostoja
    Luotu hakemista dokumenteille sekä kuville ja siirretty tiedostot oikeisiin hakemistoihin

--- GRO_raportti.ps1
+++
@@ -1,5 +0,0 @@
-﻿# Skripti tuottaa HTML-pohjaisen tiedoston ryhmäkäytännöistä
-# Kysytään polku ja tiedoston nimi:
-$Tiedosto = Read-Host "Mihin tallennetaan, anna polku ja nimi"
-# Luodaan kaikki GPO:t sisältävä HTML- raportti
-Get-GPOReport -All -ReportType html -Path $Tiedosto
